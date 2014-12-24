56858d14afad11f6a15f44bcc2a775b983374a09
konjoot <konjoot@gmail.com>
2014-12-25 00:00:37 +0300

    Setup gulp for karma

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var karma = require('karma').server;
 
-gulp.task('default', function() {
-  // place code for your default task here
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
 });
