858c12f5b5a769f563b2f7ebf5f0c48ec764b5af
konjoot <konjoot@gmail.com>
2014-12-25 22:39:04 +0300

    Install colors, override gulp done callback

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var karma = require('karma').server;
+var colors = require('colors');
 
 /**
  * Run test once and exit
@@ -8,5 +9,9 @@ gulp.task('test', function (done) {
   karma.start({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
-  }, done);
+  }, function(exitStatus){
+    if (exitStatus > 0){
+      console.log('It fail!!!'.yellow.bgRed.bold);
+    }
+  });
 });
