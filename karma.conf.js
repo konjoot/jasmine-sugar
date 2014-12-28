cb14c63ca6b73b042bcfda85c0a5a07726a80c2e
konjoot <konjoot@gmail.com>
2014-12-28 20:11:07 +0300

    Remove karma-commonjs

--- karma.conf.js
+++ karma.conf.js
@@ -10,10 +10,11 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [ 'jasmine' ],
+    frameworks: [ 'jasmine', 'commonjs' ],
 
     // list of files / patterns to load in the browser
     files: [
+      'src/**/*.coffee',
       'test/**/*Spec.coffee'
     ],
 
@@ -26,7 +27,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.coffee': ['coffee']
+      '**/*.coffee': ['coffee'],
+      '**/*.js': ['commonjs']
     },
 
 
