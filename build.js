637e9bd45b48a53c95966b5eb8dc70b5ae24a4b4
konjoot <konjoot@gmail.com>
2015-01-03 22:02:18 +0300

    create build.js config

---
+++ build.js
@@ -0,0 +1,17 @@
+({
+  baseUrl: "./dist/src",
+  name: "main",
+  out: "./dist/JasmineSugar.js",
+  optimize: 'none',
+  onModuleBundleComplete: function (data) {
+    var fs = module.require('fs'),
+      amdclean = module.require('amdclean'),
+      outputFile = data.path,
+      cleanedCode = amdclean.clean({
+        'filePath': outputFile,
+        'transformAMDChecks': false
+      });
+
+    fs.writeFileSync(outputFile, cleanedCode);
+  }
+})
