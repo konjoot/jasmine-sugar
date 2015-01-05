81ba2f45d6c4d39a03dd86d0bade49ecb5122150
konjoot <konjoot@gmail.com>
2015-01-05 20:06:38 +0300

    main.coffee removed

--- dist/src/main.js
+++
@@ -1,7 +0,0 @@
-define(['sugar'], function(JasmineSugar) {
-  return (function(context) {
-    JasmineSugar.setup(context);
-    context.JasmineSugar = JasmineSugar;
-    return context;
-  })(this);
-});
--- src/main.coffee
+++
@@ -1,5 +0,0 @@
-define ['sugar'], (JasmineSugar)->
-  do (context = this)->
-    JasmineSugar.setup(context)
-    context.JasmineSugar = JasmineSugar
-    context
