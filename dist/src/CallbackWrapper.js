f06ea5bd91bb9f0227cf8132034033a8dc6d4d63
konjoot <konjoot@gmail.com>
2015-03-03 23:22:30 +0300

    CallbackWrapper cleaned up

--- dist/src/CallbackWrapper.js
+++ dist/src/CallbackWrapper.js
@@ -1,6 +1,6 @@
-define('CallbackWrapper', ['Store', 'Context', 'Jasmine', 'Evaluator', 'ContextFactory'], function(_Store_, _Context_, _Jasmine_, _Evaluator_, _ContextFactory_) {
-  return function(fn, Store, Context, Jasmine, Evaluator, ContextFactory) {
-    var Dump, evaluator;
+define('CallbackWrapper', ['Store', 'Context', 'Jasmine', 'Evaluator', 'CallbackFormatter'], function(_Store_, _Context_, _Jasmine_, _Evaluator_, _CallbackFormatter_) {
+  return function(fn, Store, Context, Jasmine, Evaluator, CallbackFormatter) {
+    var evaluator;
     if (Store == null) {
       Store = _Store_;
     }
@@ -13,148 +13,22 @@ define('CallbackWrapper', ['Store', 'Context', 'Jasmine', 'Evaluator', 'ContextF
     if (Evaluator == null) {
       Evaluator = _Evaluator_;
     }
-    if (ContextFactory == null) {
-      ContextFactory = _ContextFactory_;
+    if (CallbackFormatter == null) {
+      CallbackFormatter = _CallbackFormatter_;
     }
     evaluator = new Evaluator();
-    Dump = function(size) {
-      var dump;
-      if (size == null) {
-        size = 9;
-      }
-      dump = [];
-      return {
-        push: function(val) {
-          if (dump.push(val) > size) {
-            return dump.shift();
-          }
-        },
-        buffer: function(index) {
-          if (index == null) {
-            return dump.join('');
-          }
-          return dump.join('')[index];
-        }
-      };
-    };
     this.prepareCallback = function() {
-      var Result, ResultFormatter, analize, beginMatched, beginWrap, callbackBegins, char, dump, endMatched, endOfLine, endWrap, inCallback, inDSLParams, inParenthesis, inString, parentheses, strings, _i, _len, _ref;
-      inString = endOfLine = endMatched = inCallback = inDSLParams = beginMatched = inParenthesis = callbackBegins = void 0;
-      parentheses = [];
-      strings = [];
-      dump = Dump();
-      ResultFormatter = function() {
-        var clearLine, describeReplacer, factoryReplacer, line, mainReplacer, offset, pushToResult, result_string;
-        result_string = [];
-        line = [];
-        offset = '';
-        clearLine = function() {
-          return line = [];
-        };
-        factoryReplacer = function(match, p1) {
-          if (p1 == null) {
-            return;
-          }
-          if (p1.length < 1) {
-            return match;
-          }
-          return match.replace(p1, p1 + offset);
-        };
-        mainReplacer = function(match, p1, p2) {
-          if (!((p1 != null) && (p2 != null))) {
-            return;
-          }
-          offset = p1;
-          return ("" + p1 + "var " + p2 + " = void 0;\n") + ("" + p1 + "var _" + p2 + "_ = new (" + (ContextFactory.toString().replace(/(\s*){1}.*/g, factoryReplacer)) + ")('" + p2 + "', evaluator, Jasmine, Store);\n") + match.replace(p2, "_" + p2 + "_");
-        };
-        describeReplacer = function(match, p1) {
-          if (p1 != null) {
-            return match.replace(p1, "_" + p1 + "_");
-          }
-        };
-        pushToResult = function() {
-          var joined_line;
-          joined_line = line.join('');
-          joined_line = joined_line.replace(/(\s*)(\w*)\.is\(.*/g, mainReplacer);
-          joined_line = joined_line.replace(/.*(describe)\(.*/g, describeReplacer);
-          joined_line = joined_line.replace(/.*([xfd]{1}describe)\(.*/g, describeReplacer);
-          return result_string.push(joined_line) && clearLine();
-        };
-        return {
-          push: function(char) {
-            line.push(char);
-            if (endOfLine != null) {
-              return pushToResult();
-            }
-          },
-          result: function() {
-            pushToResult();
-            return result_string.join('');
-          }
-        };
-      };
-      analize = function(char) {
-        dump.push(char);
-        if ((inString != null) && inString === false) {
-          inString = void 0;
-        }
-        if (endOfLine != null) {
-          endOfLine = void 0;
-        }
-        if (endMatched != null) {
-          endMatched = void 0;
-        }
-        if ((inDSLParams != null) && inDSLParams === false) {
-          inDSLParams = void 0;
-        }
-        if (beginMatched != null) {
-          beginMatched = void 0;
-        }
-        if (callbackBegins != null) {
-          callbackBegins = void 0;
-        }
-        switch (false) {
-          case !(dump.buffer() === 'tion () {' && (inCallback == null)):
-            return callbackBegins = inCallback = true;
-          case !(char === '(' && (inDSLParams != null) && (inString == null)):
-            return parentheses.push(char);
-          case !(char === ')' && (inDSLParams != null) && (inString == null)):
-            inDSLParams = parentheses.pop();
-            if (inDSLParams == null) {
-              return endMatched = true;
-            }
-            break;
-          case !(dump.buffer().substring(8) === "\n" && (inString == null)):
-            return endOfLine = true;
-          case !(dump.buffer().substring(5) === '.is(' && (typeof inDescribe === "undefined" || inDescribe === null)):
-            return inDSLParams = beginMatched = true;
-          case !((char.match(/'|"/) != null) && (inDSLParams != null) && strings.indexOf(char) < 0 && !dump.buffer(7) === "\\"):
-            strings.push(char);
-            return inString = true;
-          case !((char.match(/'|"/) != null) && (inDSLParams != null) && !dump.buffer(7) === "\\"):
-            strings.splice(strings.indexOf(char), 1);
-            return inString = strings.length > 0;
-        }
-      };
+      var Formatter, char, _i, _len, _ref;
       if (fn == null) {
         return '';
       }
-      Result = ResultFormatter();
-      beginWrap = 'function() { return ';
-      endWrap = '; }';
+      Formatter = CallbackFormatter();
       _ref = fn.toString();
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         char = _ref[_i];
-        analize(char);
-        if (endMatched != null) {
-          Result.push(endWrap);
-        }
-        Result.push(char);
-        if (beginMatched != null) {
-          Result.push(beginWrap);
-        }
+        Formatter.push(char);
       }
-      return eval("(" + (Result.result()) + ");");
+      return eval("(" + (Formatter.result()) + ");");
     };
     this.run = function() {
       return this.prepareCallback().call(Context.get());
--- src/CallbackWrapper.coffee
+++ src/CallbackWrapper.coffee
@@ -3,142 +3,30 @@ define 'CallbackWrapper',
  'Context',
  'Jasmine',
  'Evaluator',
- 'ContextFactory'],
+ 'CallbackFormatter'],
 (_Store_,
  _Context_,
  _Jasmine_,
  _Evaluator_,
- _ContextFactory_)->
+ _CallbackFormatter_)->
 
   (fn,
    Store = _Store_,
    Context = _Context_,
    Jasmine = _Jasmine_,
    Evaluator = _Evaluator_,
-   ContextFactory = _ContextFactory_)->
+   CallbackFormatter = _CallbackFormatter_)->
 
     evaluator = new Evaluator()
 
-    Dump = (size = 9)->
-      dump = []
-
-      {
-        push: (val)->
-          dump.shift() if dump.push(val) > size
-
-        buffer: (index)->
-          return dump.join('') unless index?
-          dump.join('')[index]
-      }
-
     @prepareCallback = ->
-      inString       =
-      endOfLine      =
-      endMatched     =
-      inCallback     =
-      inDSLParams    =
-      beginMatched   =
-      inParenthesis  =
-      callbackBegins = undefined
-
-      parentheses  = []
-      strings      = []
-
-      dump = Dump()
-
-
-      ResultFormatter = ->
-        result_string = []
-        line = []
-        offset = ''
-
-        clearLine = -> line = []
-
-        factoryReplacer = (match, p1)->
-          return unless p1?
-          return match if p1.length < 1
-          match.replace(p1, p1 + offset)
-
-        mainReplacer = (match, p1, p2)->
-          return unless p1? && p2?
-          offset = p1
-          "#{p1}var #{p2} = void 0;\n" +
-          "#{p1}var _#{p2}_ = new (#{ContextFactory.toString().replace(/(\s*){1}.*/g, factoryReplacer)})('#{p2}', evaluator, Jasmine, Store);\n" +
-          match.replace(p2, "_#{p2}_")
-
-        describeReplacer = (match, p1)->
-          match.replace(p1, "_#{p1}_") if p1?
-
-        pushToResult = ->
-          joined_line = line.join('')
-          joined_line = joined_line.replace(/(\s*)(\w*)\.is\(.*/g, mainReplacer)
-          joined_line = joined_line.replace(/.*(describe)\(.*/g, describeReplacer)
-          joined_line = joined_line.replace(/.*([xfd]{1}describe)\(.*/g, describeReplacer)
-          result_string.push(joined_line) and clearLine()
-
-        {
-          push: (char)->
-            line.push(char)
-            pushToResult() if endOfLine?
-
-          result: ->
-            pushToResult()
-            result_string.join('')
-        }
-
-      analize = (char)->
-        dump.push char
-        inString = undefined if inString? and inString == false
-        endOfLine = undefined if endOfLine?
-        endMatched = undefined if endMatched?
-        inDSLParams = undefined if inDSLParams? and inDSLParams == false
-        beginMatched = undefined if beginMatched?
-        callbackBegins = undefined if callbackBegins?
-
-        switch
-          when dump.buffer() == 'tion () {' and not inCallback?
-            callbackBegins = inCallback = true
-          when char == '(' and inDSLParams? and not inString?
-            parentheses.push char
-          when char == ')' and inDSLParams? and not inString?
-            inDSLParams = parentheses.pop()
-            endMatched = true unless inDSLParams?
-          when dump.buffer().substring(8) == "\n" and not inString?
-            endOfLine = true
-          when dump.buffer().substring(5) == '.is(' and not inDescribe?
-            inDSLParams = beginMatched = true
-          when char.match(/'|"/)? and inDSLParams? and strings.indexOf(char) < 0 and not dump.buffer(7) == "\\"
-            strings.push(char)
-            inString = true
-          when char.match(/'|"/)? and inDSLParams? and not dump.buffer(7) == "\\"
-            strings.splice(strings.indexOf(char), 1)
-            inString = strings.length > 0
-
       return '' unless fn?
 
-      Result = ResultFormatter()
+      Formatter = CallbackFormatter()
 
-      beginWrap = 'function() { return '
-      endWrap   = '; }'
+      Formatter.push(char) for char in fn.toString()
 
-      for char in fn.toString()
-        analize char
-        Result.push(endWrap) if endMatched?
-        Result.push char
-        Result.push(beginWrap) if beginMatched?
-
-      # planning refactoring
-      # analize = (char)->
-      #   new Analizer
-
-      # for char in fn.toString()
-      #   analize(char)
-      #     .insertATrailingWrap()
-      #     .save()
-      #     .insertDslObjects()
-      #     .insertAnOpeningWrap()
-
-      eval("(#{Result.result()});")
+      eval("(#{Formatter.result()});")
 
     @run = ->
       @prepareCallback().call Context.get()
